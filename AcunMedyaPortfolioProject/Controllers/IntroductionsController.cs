fc77e121b977e1d115fc641dfb5236b8429dd799
Sevval-Demir <sevvaldemirrt@gmail.com>
2025-02-26 11:50:59 +0300

    revize edildi

--- AcunMedyaPortfolioProject/Controllers/IntroductionsController.cs
+++ AcunMedyaPortfolioProject/Controllers/IntroductionsController.cs
@@ -6,15 +6,16 @@ using System.Web.Mvc;
 using AcunMedyaPortfolioProject.Models;
 namespace AcunMedyaPortfolioProject.Controllers
 {
-    public class IntroductıonsController : Controller
+    public class IntroductionsController : Controller
     {
-        // GET: Introductıons
-        PortfolioDbEntities1 db=new PortfolioDbEntities1();
+        // GET: Introductions
+        PortfolioDbEntities1 db = new PortfolioDbEntities1();
         public ActionResult Index()
         {
             var value = db.Introductions.ToList();
             return View(value);
         }
+
         [HttpGet]
         public ActionResult CreateIntroduction()
         {
@@ -22,31 +23,32 @@ namespace AcunMedyaPortfolioProject.Controllers
         }
 
         [HttpPost]
-        public ActionResult CreateIntroduction(Introductions ınt)
+        public ActionResult CreateIntroduction(Introductions value)
         {
-            db.Introductions.Add(ınt);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+                db.Introductions.Add(value);
+                db.SaveChanges();
+                return RedirectToAction("Index");
         }
         public ActionResult DeleteIntroduction(int id)
         {
-            var value = db.Introductions.Find(id);
-            db.Introductions.Remove(value);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+                var value = db.Introductions.Find(id);
+                db.Introductions.Remove(value);
+                db.SaveChanges();
+                return RedirectToAction("Index");
         }
         [HttpGet]
         public ActionResult UpdateIntroduction(int id)
         {
             var value = db.Introductions.Find(id);
-           return View(value);
+            return View(value);
         }
+
         [HttpPost]
-        public ActionResult UpdateIntroduction(Introductions ınt)
+        public ActionResult UpdateIntroduction(Introductions i)
         {
-            var value = db.Introductions.Find(ınt.Id);
-            value.Title = ınt.Title;
-            value.Description=ınt.Description;
+            var value = db.Introductions.Find(i.Id);
+                value.Title =i.Title;
+                value.Description = i.Description;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
---
+++ AcunMedyaPortfolioProject/Controllers/LayoutController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcunMedyaPortfolioProject.Controllers
+{
+    public class LayoutController : Controller
+    {
+        // GET: Layout
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialScripts()
+        {
+            return PartialView();
+        }
+    }
+}
